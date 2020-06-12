2245dc5b86ab34cde6363e2638b67faf231121a8
Allie Robinson <allierob93@gmail.com>
2020-06-12 11:05:47 -0500

    met mvp

--- adv.py
+++ adv.py
@@ -17,7 +17,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -29,7 +29,43 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+visited_rooms = dict()
+path = [] # chronological list of where player has recently moved (used for backtracking)
+reverse = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'} # reference for opposite direction
 
+visited_rooms[player.current_room.id] = player.current_room.get_exits()
+
+# we want the number of visited rooms to be the same as the number of rooms
+while len(visited_rooms) < len(room_graph):
+    # if the room hasn't been visited
+    if player.current_room.id not in visited_rooms:
+        # add this room to visited rooms
+        visited_rooms[player.current_room.id] = player.current_room.get_exits()
+        # set current move to where player went according to backtracker
+        current_move = path[-1]
+        # remove current move from the visited room so that we don't revisit it
+        visited_rooms[player.current_room.id].remove(current_move)
+    # if there are no exits (or all exits have been explored)
+    if len(visited_rooms[player.current_room.id]) == 0:
+        # set current move to where player went according to backtracker
+        current_move = path[-1]
+        # remove the last move from path
+        path.pop()
+        # add the current move to the final traversal path
+        traversal_path.append(current_move)
+        # player travels backwards to get out of room with no more unexplored exits
+        player.travel(current_move)
+    else:
+        # pick an unexplored exit
+        next_room = visited_rooms[player.current_room.id][-1]
+        # remove the exit because it is now going to be explored
+        visited_rooms[player.current_room.id].pop()
+        # move player to the next room
+        player.travel(next_room)
+        # add the movement to the backtracker path
+        path.append(reverse[next_room])
+        # add the movement to the final path
+        traversal_path.append(next_room)
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
@@ -41,16 +77,17 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
-#######
+#######ls
+
 player.current_room.print_room_description(player)
 while True:
     cmds = input("-> ").lower().split(" ")
